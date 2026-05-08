41fef5aa61bff5aefc4df8c4bae5f82c14071731
James Mancilla <jamesmv1986@gmail.com>
2026-05-08 15:43:21 -0500

    Fix puerto Railway

--- src/index.js
+++ src/index.js
@@ -111,7 +111,7 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes)
 
 // Puerto
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Servidor Goevly corriendo en puerto ${PORT}`)
 })
